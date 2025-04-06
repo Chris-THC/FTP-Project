e1a59186469051e5390afef13ae184c22ecd2018
Cris <chris_49git@outlook.com>
2025-04-06 17:05:28 -0600

    Project started

---
+++ src/main/java/com/ftp/api/FtpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ftp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtpProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtpProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ftp/api/FtpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtpProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
